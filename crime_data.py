1dfdb3ec9fbaac2b13fb87cd015080b7c772bc1e
bauyrzha <90171254+bauyrzha@users.noreply.github.com>
2022-03-25 16:25:43 -0700

    Update crime_data.py

--- crime_data.py
+++ crime_data.py
@@ -6,8 +6,8 @@ st.title('Uber pickups in NYC')
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://data.lacity.org/api/views/2nrs-mtv8/rows.csv?accessType=DOWNLOAD')
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows, error_bad_lines=False)
+def load_data:
+    data = pd.read_csv(DATA_URL, error_bad_lines=False)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
@@ -16,7 +16,7 @@ def load_data(nrows):
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+data = load_data
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache)")
 if st.checkbox('Show raw data'):
@@ -28,5 +28,5 @@ if st.checkbox('Show raw data'):
 #st.bar_chart(hist_values)
 #hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 #filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-#st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-#st.map(filtered_data)
+st.subheader(f'Map of all crimes')
+st.map(data)
