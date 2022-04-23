9398bd7b5baeb190e14f521324ac2692d6978822
bauyrzha <90171254+bauyrzha@users.noreply.github.com>
2022-04-22 19:27:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,9 @@ def homepage():
         #### ***Topic:*** This project will examine inequalities in regions of Los Angeles based on crime rates, access to food, and access to quality education. 
         ###### The app is made by _**Yerkebulan B., Soumeya K., Zihao H.**_
         #""")
-    image = Image.open('homepage.jpg')
-    st.image(image, caption='Original Map of Los Angeles Country')
+    try:
+        image = Image.open('homepage.jpg')
+        st.image(image, caption='Original Map of Los Angeles Country')
 
 def crime():
     st.title('Crime in LA')
